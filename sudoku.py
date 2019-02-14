0515d405b0d1001ad4a963a7e96c1eed96f182ee
Unknown <jua16073@uvg.edu.gt>
2019-02-14 01:44:37 -0600

    Win Condition
    Muy mal optimizada

--- sudoku.py
+++ sudoku.py
@@ -15,6 +15,7 @@ def sudoku(argv):
   print(mat)
   printing(mat)
   #p.problema(mat)
+  print(win_Condition(mat))
   actions(mat)
 
 
@@ -43,7 +44,7 @@ def printing(matriz):
   print("")
 
 ## Funcion para designar a cual paso darle prioridad
-def heuristica(matriz):
+def heuristica1(matriz):
   points = 0
   for x in range(4):
     nums = 0
@@ -84,10 +85,57 @@ def actions(matriz):
     for x in range(4):
       if (matriz[x][posiciones[cont][1]] != "." and (matriz[x][posiciones[cont][1]] in nums) ):
         nums.remove(matriz[x][posiciones[cont][1]])
+    posibilidades.append(nums)
     print(nums)
-
     cont += 1
 
+def win_Condition(matriz):
+  # Comprobar si estan todos en las filas
+  for x in range(4):
+    nums = ["1", "2", "3", "4"]
+    for y in range(4):
+      if (matriz[x][y] in nums):
+        nums.remove(matriz[x][y])
+    if (len(nums) != 0):
+      return False
+
+  #Comprobar si estan todos en las columnas
+  for x in range(4):
+    nums = ["1", "2", "3", "4"]
+    for y in range(4):
+      if (matriz[y][x] in nums):
+        nums.remove(matriz[y][x])
+    if (len(nums) != 0):
+      return False
+
+  #Comprobar si estan todos en los cuadros 
+  h = 0
+  k = 0
+  i = 0
+  while (i < 4):
+    if (i == 1):
+      k = 2
+    if (i == 2):
+      k = 0
+      h = 2
+    if (i == 3):
+      k = 2
+      h = 2
+    for x in range(2):
+      for y in range(2):
+        if( matriz[x+h][y+k] in nums ):
+          nums.remove(matriz[x+h][y+k])
+    if (len(nums) != 0):
+        return False
+    i +=1 
+
+  return True
+    
+  
+  
+
+
+##Para empezar 
 if __name__ == "__main__":
   sudoku(sys.argv)
   pass
