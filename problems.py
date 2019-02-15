3bca23e7750a4a5908ff9506c590a71676d474fb
Unknown <jua16073@uvg.edu.gt>
2019-02-15 03:27:15 -0600

    Totito funcional

--- problems.py
+++ problems.py
@@ -1,29 +1,33 @@
 # archivo para generar el grafo del problema.
 # inicio, acciones, heuristica
+import copy
 
 
 def graph_search(problema):
     frontera = []
     explorado = []
+    path = []
     estado = problema.inicial
-    frontera.append(estado)
+    path.append(estado)
+    frontera.append(path)
 
     while True:
         if len(frontera):
-            path = problema.criterio(frontera)
-            frontera.remove(path)
+            path = problema.criteria(frontera)
             estado = path[len(path)-1]
-            explorado.append(frontera[len(frontera)-1])
+            explorado.append(estado)
+            frontera.remove(path)
 
-        if problema.win_condition(estado):
-            return path
 
-        probabilidades = problema.acciones(estado)
-        for a in probabilidades:
-            if a not in explorado:
-                new_path = path
-                new_path.append(a)
-                frontera.append(new_path)
+            if problema.win_condition(estado):
+                return path
+
+            probabilidades = problema.actions(estado)
+            for a in probabilidades:
+                if a not in explorado:
+                    new_path = copy.deepcopy(path)
+                    new_path.append(a)
+                    frontera.append(new_path)
         else:
             return False
 
--- sudoku.py
+++ sudoku.py
@@ -15,10 +15,120 @@ def sudoku(argv):
         print("No es un sudoku de 4x4")
         return 0
     mat = a_matriz(argv[1])
-    print(mat)
-    printing(mat)
-    # p.problema(mat)
-    actions(mat)
+    problem = sudoku_problem()
+    problem.inicial = mat
+
+    respuesta = p.graph_search(problem)
+    if not respuesta:
+        print(respuesta)
+    else:
+        for x in respuesta:
+            printing(x)
+
+class sudoku_problem:
+    inicial = "Colocar estado inicial"
+
+    def actions(self,matriz):
+        espacios = 0
+        posiciones = []
+        posibilidades = []
+        matrices = []
+        # Encontrar cuantos espacios vacios hay
+        for x in range(4):
+            for y in range(4):
+                if matriz[x][y] == ".":
+                    posiciones.append(tuple([x, y]))
+                    espacios += 1
+
+        # Sacar cada una de las posibilidades
+        cont = 0
+        while cont < espacios:
+            nums = ["1", "2", "3", "4"]
+            for x in range(4):
+                if matriz[posiciones[cont][0]][x] != ".":
+                    nums.remove(matriz[posiciones[cont][0]][x])
+
+            for x in range(4):
+                if matriz[x][posiciones[cont][1]] != "." and (matriz[x][posiciones[cont][1]] in nums):
+                    nums.remove(matriz[x][posiciones[cont][1]])
+            posibilidades.append(nums)
+            cont += 1
+
+        cont = 0
+        while cont < espacios:
+            for x in posibilidades[cont]:
+                nueva_matriz = copy.deepcopy(matriz)
+                nueva_matriz[posiciones[cont][0]][posiciones[cont][1]] = x
+                matrices.append(nueva_matriz)
+            cont += 1
+        return matrices
+
+    def win_condition(self, matriz):
+        # Comprobar si estan todos en las filas
+        for x in range(4):
+            nums = ["1", "2", "3", "4"]
+            for y in range(4):
+                if matriz[x][y] in nums:
+                    nums.remove(matriz[x][y])
+            if len(nums) != 0:
+                return False
+
+        # Comprobar si estan todos en las columnas
+        for x in range(4):
+            nums = ["1", "2", "3", "4"]
+            for y in range(4):
+                if matriz[y][x] in nums:
+                    nums.remove(matriz[y][x])
+            if len(nums) != 0:
+                return False
+
+        # Comprobar si estan todos en los cuadros
+        h = 0
+        k = 0
+        i = 0
+        while i < 4:
+            if i == 1:
+                k = 2
+            if i == 2:
+                k = 0
+                h = 2
+            if i == 3:
+                k = 2
+                h = 2
+            for x in range(2):
+                for y in range(2):
+                    if matriz[x + h][y + k] in nums:
+                        nums.remove(matriz[x + h][y + k])
+            if len(nums) != 0:
+                return False
+            i += 1
+
+        return True
+
+    def criteria(self,paths):
+        path_seleccionado = None;
+        points_winner = 0
+        for x in paths:
+            estado = x[len(x) - 1]
+            points = 0
+
+            for h in range(4):
+                nums = 0
+                for y in range(4):
+                    if estado[h][y] != ".":
+                        nums += 1
+                points += nums * nums
+
+            for h in range(4):
+                nums = 0
+                for y in range(4):
+                    if estado[y][h] != ".":
+                        nums += 1
+                points += nums * nums
+            if points > points_winner:
+                path_seleccionado = x
+                points_winner = points
+        return path_seleccionado
 
 
 # Funcion para pasar de un vector a una matriz
@@ -44,112 +154,6 @@ def printing(matriz):
             print("\n___________________\n", end="")
         print(matriz[int(x / 4)][x % 4] + "  | ", end='')
     print("")
-
-
-## Funcion para designar a cual paso darle prioridad
-def heuristica1(matriz):
-    points = 0
-    for x in range(4):
-        nums = 0
-        for y in range(4):
-            if matriz[x][y] != ".":
-                nums += 1
-        points += nums * nums
-
-    for x in range(4):
-        nums = 0
-        for y in range(4):
-            if matriz[y][x] != ".":
-                nums += 1
-        points += nums * nums
-    return points
-
-
-def actions(matriz):
-    espacios = 0
-    posiciones = []
-    posibilidades = []
-    matrices = []
-    # Encontrar cuantos espacios vacios hay
-    for x in range(4):
-        for y in range(4):
-            if matriz[x][y] == ".":
-                posiciones.append(tuple([x, y]))
-                espacios += 1
-    print(posiciones)
-
-    # Sacar cada una de las posibilidades
-    cont = 0
-    while cont < espacios:
-        nums = ["1", "2", "3", "4"]
-        for x in range(4):
-            if matriz[posiciones[cont][0]][x] != ".":
-                nums.remove(matriz[posiciones[cont][0]][x])
-
-        for x in range(4):
-            if matriz[x][posiciones[cont][1]] != "." and (matriz[x][posiciones[cont][1]] in nums):
-                nums.remove(matriz[x][posiciones[cont][1]])
-        posibilidades.append(nums)
-        print(nums)
-        cont += 1
-
-    cont = 0
-    while cont < espacios:
-        for x in posibilidades[cont]:
-            nueva_matriz = copy.deepcopy(matriz)
-            nueva_matriz[posiciones[cont][0]][posiciones[cont][1]] = x
-            matrices.append(nueva_matriz)
-        cont += 1
-    return matrices
-
-def win_condition(matriz):
-    # Comprobar si estan todos en las filas
-    for x in range(4):
-        nums = ["1", "2", "3", "4"]
-        for y in range(4):
-            if matriz[x][y] in nums:
-                nums.remove(matriz[x][y])
-        if len(nums) != 0:
-            return False
-
-    # Comprobar si estan todos en las columnas
-    for x in range(4):
-        nums = ["1", "2", "3", "4"]
-        for y in range(4):
-            if matriz[y][x] in nums:
-                nums.remove(matriz[y][x])
-        if len(nums) != 0:
-            return False
-
-    # Comprobar si estan todos en los cuadros
-    h = 0
-    k = 0
-    i = 0
-    while i < 4:
-        if i == 1:
-            k = 2
-        if i == 2:
-            k = 0
-            h = 2
-        if i == 3:
-            k = 2
-            h = 2
-        for x in range(2):
-            for y in range(2):
-                if matriz[x + h][y + k] in nums:
-                    nums.remove(matriz[x + h][y + k])
-        if len(nums) != 0:
-            return False
-        i += 1
-
-    return True
-
-def criteria(paths):
-    path_seleccionado = None;
-    for x in paths:
-        estado = x[len(x)-1]
-        
-
 
 
 # Para empezar
